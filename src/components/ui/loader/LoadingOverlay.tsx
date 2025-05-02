6912268d596d595dee61628e52a25838ff9a7c27
Pradip Puri <impradp@gmail.com>
2025-05-02 19:03:22 +0545

    enhancement/CRM-80: Remove background color for loader compoennt.

--- src/components/ui/loader/LoadingOverlay.tsx
+++ src/components/ui/loader/LoadingOverlay.tsx
@@ -1,11 +1,25 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import LoadingSpinner from "@/components/ui/loader/LoadingSpinner";
 
 const LoadingOverlay = ({ size = 50 }: { size?: number }) => {
+  // Prevent background scrolling when the overlay is mounted
+  useEffect(() => {
+    // Save original overflow style
+    const originalOverflow = document.body.style.overflow;
+
+    // Disable scrolling
+    document.body.style.overflow = "hidden";
+
+    // Re-enable scrolling when component unmounts
+    return () => {
+      document.body.style.overflow = originalOverflow;
+    };
+  }, []);
+
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+    <div className="fixed inset-0 z-50 flex items-center justify-center">
       <LoadingSpinner size={size} />
     </div>
   );
