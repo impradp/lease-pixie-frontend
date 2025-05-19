dedc368ad3e7eda7acbac4de627654ba2e3ee834
Pradip Puri <impradp@gmail.com>
2025-05-19 14:50:44 +0545

    fix-loader/dots-fade: Simulating dots-fade as loader.

--- src/components/ui/loader/LoadingOverlay.tsx
+++ src/components/ui/loader/LoadingOverlay.tsx
@@ -1,12 +1,46 @@
 "use client";
 
 import React from "react";
-import LoadingSpinner from "@/components/ui/loader/LoadingSpinner";
 
-const LoadingOverlay = ({ size = 50 }: { size?: number }) => {
+const LoadingOverlay = () => {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center">
-      <LoadingSpinner size={size} />
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={48}
+        height={48}
+        viewBox="0 0 24 24"
+      >
+        <circle cx={4} cy={12} r={3} fill="#aa5a36">
+          <animate
+            id="svgSpinners3DotsFade0"
+            fill="freeze"
+            attributeName="opacity"
+            begin="0;svgSpinners3DotsFade1.end-0.313s"
+            dur="0.938s"
+            values="1;0.2"
+          ></animate>
+        </circle>
+        <circle cx={12} cy={12} r={3} fill="#aa5a36" opacity={0.4}>
+          <animate
+            fill="freeze"
+            attributeName="opacity"
+            begin="svgSpinners3DotsFade0.begin+0.188s"
+            dur="0.938s"
+            values="1;0.2"
+          ></animate>
+        </circle>
+        <circle cx={20} cy={12} r={3} fill="#aa5a36" opacity={0.3}>
+          <animate
+            id="svgSpinners3DotsFade1"
+            fill="freeze"
+            attributeName="opacity"
+            begin="svgSpinners3DotsFade0.begin+0.375s"
+            dur="0.938s"
+            values="1;0.2"
+          ></animate>
+        </circle>
+      </svg>
     </div>
   );
 };
