bf0ad59f05caea608f5a74d16588d678358c064b
Pradip Puri <impradp@gmail.com>
2025-05-19 14:00:18 +0545

    fix-loader/bars-scale: Simulating bars-scale as loader.

--- src/components/ui/loader/LoadingOverlay.tsx
+++ src/components/ui/loader/LoadingOverlay.tsx
@@ -1,12 +1,88 @@
 "use client";
 
 import React from "react";
-import LoadingSpinner from "@/components/ui/loader/LoadingSpinner";
 
-const LoadingOverlay = ({ size = 50 }: { size?: number }) => {
+const LoadingOverlay = () => {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center">
-      <LoadingSpinner size={size} />
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={48}
+        height={48}
+        viewBox="0 0 24 24"
+      >
+        <rect width={6} height={14} x={1} y={4} fill="#aa5a36">
+          <animate
+            id="svgSpinnersBarsScaleFade0"
+            fill="freeze"
+            attributeName="y"
+            begin="0;svgSpinnersBarsScaleFade1.end-0.313s"
+            dur="0.938s"
+            values="1;5"
+          ></animate>
+          <animate
+            fill="freeze"
+            attributeName="height"
+            begin="0;svgSpinnersBarsScaleFade1.end-0.313s"
+            dur="0.938s"
+            values="22;14"
+          ></animate>
+          <animate
+            fill="freeze"
+            attributeName="opacity"
+            begin="0;svgSpinnersBarsScaleFade1.end-0.313s"
+            dur="0.938s"
+            values="1;0.2"
+          ></animate>
+        </rect>
+        <rect width={6} height={14} x={9} y={4} fill="#aa5a36" opacity={0.4}>
+          <animate
+            fill="freeze"
+            attributeName="y"
+            begin="svgSpinnersBarsScaleFade0.begin+0.188s"
+            dur="0.938s"
+            values="1;5"
+          ></animate>
+          <animate
+            fill="freeze"
+            attributeName="height"
+            begin="svgSpinnersBarsScaleFade0.begin+0.188s"
+            dur="0.938s"
+            values="22;14"
+          ></animate>
+          <animate
+            fill="freeze"
+            attributeName="opacity"
+            begin="svgSpinnersBarsScaleFade0.begin+0.188s"
+            dur="0.938s"
+            values="1;0.2"
+          ></animate>
+        </rect>
+        <rect width={6} height={14} x={17} y={4} fill="#aa5a36" opacity={0.3}>
+          <animate
+            id="svgSpinnersBarsScaleFade1"
+            fill="freeze"
+            attributeName="y"
+            begin="svgSpinnersBarsScaleFade0.begin+0.375s"
+            dur="0.938s"
+            values="1;5"
+          ></animate>
+          <animate
+            fill="freeze"
+            attributeName="height"
+            begin="svgSpinnersBarsScaleFade0.begin+0.375s"
+            dur="0.938s"
+            values="22;14"
+          ></animate>
+          <animate
+            fill="freeze"
+            attributeName="opacity"
+            begin="svgSpinnersBarsScaleFade0.begin+0.375s"
+            dur="0.938s"
+            values="1;0.2"
+          ></animate>
+        </rect>
+      </svg>
     </div>
   );
 };
