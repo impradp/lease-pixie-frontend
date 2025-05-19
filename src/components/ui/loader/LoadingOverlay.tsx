4fd948d51c9b521a22331a1462c5ef638b0811d1
Pradip Puri <impradp@gmail.com>
2025-05-19 15:23:26 +0545

    fix-loader/blocks-shuffle: Simulating blocks-shuffle as loader.

--- src/components/ui/loader/LoadingOverlay.tsx
+++ src/components/ui/loader/LoadingOverlay.tsx
@@ -1,12 +1,119 @@
 "use client";
 
 import React from "react";
-import LoadingSpinner from "@/components/ui/loader/LoadingSpinner";
 
-const LoadingOverlay = ({ size = 50 }: { size?: number }) => {
+const LoadingOverlay = () => {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center">
-      <LoadingSpinner size={size} />
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={48}
+        height={48}
+        viewBox="0 0 24 24"
+      >
+        <rect width={10} height={10} x={1} y={1} fill="#aa5a36" rx={1}>
+          <animate
+            id="svgSpinnersBlocksShuffle30"
+            fill="freeze"
+            attributeName="x"
+            begin="0;svgSpinnersBlocksShuffle3b.end"
+            dur="0.25s"
+            values="1;13"
+          ></animate>
+          <animate
+            id="svgSpinnersBlocksShuffle31"
+            fill="freeze"
+            attributeName="y"
+            begin="svgSpinnersBlocksShuffle38.end"
+            dur="0.25s"
+            values="1;13"
+          ></animate>
+          <animate
+            id="svgSpinnersBlocksShuffle32"
+            fill="freeze"
+            attributeName="x"
+            begin="svgSpinnersBlocksShuffle39.end"
+            dur="0.25s"
+            values="13;1"
+          ></animate>
+          <animate
+            id="svgSpinnersBlocksShuffle33"
+            fill="freeze"
+            attributeName="y"
+            begin="svgSpinnersBlocksShuffle3a.end"
+            dur="0.25s"
+            values="13;1"
+          ></animate>
+        </rect>
+        <rect width={10} height={10} x={1} y={13} fill="#aa5a36" rx={1}>
+          <animate
+            id="svgSpinnersBlocksShuffle34"
+            fill="freeze"
+            attributeName="y"
+            begin="svgSpinnersBlocksShuffle30.end"
+            dur="0.25s"
+            values="13;1"
+          ></animate>
+          <animate
+            id="svgSpinnersBlocksShuffle35"
+            fill="freeze"
+            attributeName="x"
+            begin="svgSpinnersBlocksShuffle31.end"
+            dur="0.25s"
+            values="1;13"
+          ></animate>
+          <animate
+            id="svgSpinnersBlocksShuffle36"
+            fill="freeze"
+            attributeName="y"
+            begin="svgSpinnersBlocksShuffle32.end"
+            dur="0.25s"
+            values="1;13"
+          ></animate>
+          <animate
+            id="svgSpinnersBlocksShuffle37"
+            fill="freeze"
+            attributeName="x"
+            begin="svgSpinnersBlocksShuffle33.end"
+            dur="0.25s"
+            values="13;1"
+          ></animate>
+        </rect>
+        <rect width={10} height={10} x={13} y={13} fill="#aa5a36" rx={1}>
+          <animate
+            id="svgSpinnersBlocksShuffle38"
+            fill="freeze"
+            attributeName="x"
+            begin="svgSpinnersBlocksShuffle34.end"
+            dur="0.25s"
+            values="13;1"
+          ></animate>
+          <animate
+            id="svgSpinnersBlocksShuffle39"
+            fill="freeze"
+            attributeName="y"
+            begin="svgSpinnersBlocksShuffle35.end"
+            dur="0.25s"
+            values="13;1"
+          ></animate>
+          <animate
+            id="svgSpinnersBlocksShuffle3a"
+            fill="freeze"
+            attributeName="x"
+            begin="svgSpinnersBlocksShuffle36.end"
+            dur="0.25s"
+            values="1;13"
+          ></animate>
+          <animate
+            id="svgSpinnersBlocksShuffle3b"
+            fill="freeze"
+            attributeName="y"
+            begin="svgSpinnersBlocksShuffle37.end"
+            dur="0.25s"
+            values="1;13"
+          ></animate>
+        </rect>
+      </svg>
     </div>
   );
 };
