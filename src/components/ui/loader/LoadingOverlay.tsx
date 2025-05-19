5d9c14c7058c331bd4d9a4a0711e6850491e5b85
Pradip Puri <impradp@gmail.com>
2025-05-19 15:01:21 +0545

    fix-loader/dots-rotate: Simulating dots-rotate as loader.

--- src/components/ui/loader/LoadingOverlay.tsx
+++ src/components/ui/loader/LoadingOverlay.tsx
@@ -1,12 +1,34 @@
 "use client";
 
 import React from "react";
-import LoadingSpinner from "@/components/ui/loader/LoadingSpinner";
 
-const LoadingOverlay = ({ size = 50 }: { size?: number }) => {
+const LoadingOverlay = () => {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center">
-      <LoadingSpinner size={size} />
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={48}
+        height={48}
+        viewBox="0 0 24 24"
+      >
+        <g>
+          <circle cx={3} cy={12} r={2} fill="#aa5a36"></circle>
+          <circle cx={21} cy={12} r={2} fill="#aa5a36"></circle>
+          <circle cx={12} cy={21} r={2} fill="#aa5a36"></circle>
+          <circle cx={12} cy={3} r={2} fill="#aa5a36"></circle>
+          <circle cx={5.64} cy={5.64} r={2} fill="#aa5a36"></circle>
+          <circle cx={18.36} cy={18.36} r={2} fill="#aa5a36"></circle>
+          <circle cx={5.64} cy={18.36} r={2} fill="#aa5a36"></circle>
+          <circle cx={18.36} cy={5.64} r={2} fill="#aa5a36"></circle>
+          <animateTransform
+            attributeName="transform"
+            dur="1.875s"
+            repeatCount="indefinite"
+            type="rotate"
+            values="0 12 12;360 12 12"
+          ></animateTransform>
+        </g>
+      </svg>
     </div>
   );
 };
