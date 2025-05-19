8808f3356b4f7e78813cbc470de9359e94492c02
Pradip Puri <impradp@gmail.com>
2025-05-19 14:38:15 +0545

    fix-loader/ring-with-bg: Simulating ring-with-bg as loader.

--- src/components/ui/loader/LoadingOverlay.tsx
+++ src/components/ui/loader/LoadingOverlay.tsx
@@ -1,12 +1,34 @@
 "use client";
 
 import React from "react";
-import LoadingSpinner from "@/components/ui/loader/LoadingSpinner";
 
-const LoadingOverlay = ({ size = 50 }: { size?: number }) => {
+const LoadingOverlay = () => {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center">
-      <LoadingSpinner size={size} />
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={48}
+        height={48}
+        viewBox="0 0 24 24"
+      >
+        <path
+          fill="#aa5a36"
+          d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z"
+          opacity={0.25}
+        ></path>
+        <path
+          fill="#aa5a36"
+          d="M12,4a8,8,0,0,1,7.89,6.7A1.53,1.53,0,0,0,21.38,12h0a1.5,1.5,0,0,0,1.48-1.75,11,11,0,0,0-21.72,0A1.5,1.5,0,0,0,2.62,12h0a1.53,1.53,0,0,0,1.49-1.3A8,8,0,0,1,12,4Z"
+        >
+          <animateTransform
+            attributeName="transform"
+            dur="0.938s"
+            repeatCount="indefinite"
+            type="rotate"
+            values="0 12 12;360 12 12"
+          ></animateTransform>
+        </path>
+      </svg>
     </div>
   );
 };
