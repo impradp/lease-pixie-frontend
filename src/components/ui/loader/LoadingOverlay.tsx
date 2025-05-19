f4f9196fd080282f8024c78db4261cc0763c9c28
Pradip Puri <impradp@gmail.com>
2025-05-19 15:16:52 +0545

    fix-loader/ring-resize: Simulating ring-resize as loader.

--- src/components/ui/loader/LoadingOverlay.tsx
+++ src/components/ui/loader/LoadingOverlay.tsx
@@ -1,12 +1,53 @@
 "use client";
 
 import React from "react";
-import LoadingSpinner from "@/components/ui/loader/LoadingSpinner";
 
-const LoadingOverlay = ({ size = 50 }: { size?: number }) => {
+const LoadingOverlay = () => {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center">
-      <LoadingSpinner size={size} />
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={48}
+        height={48}
+        viewBox="0 0 24 24"
+      >
+        <g stroke="#aa5a36" strokeWidth={1}>
+          <circle
+            cx={12}
+            cy={12}
+            r={9.5}
+            fill="none"
+            strokeLinecap="round"
+            strokeWidth={3}
+          >
+            <animate
+              attributeName="stroke-dasharray"
+              calcMode="spline"
+              dur="1.875s"
+              keySplines="0.42,0,0.58,1;0.42,0,0.58,1;0.42,0,0.58,1"
+              keyTimes="0;0.475;0.95;1"
+              repeatCount="indefinite"
+              values="0 150;42 150;42 150;42 150"
+            ></animate>
+            <animate
+              attributeName="stroke-dashoffset"
+              calcMode="spline"
+              dur="1.875s"
+              keySplines="0.42,0,0.58,1;0.42,0,0.58,1;0.42,0,0.58,1"
+              keyTimes="0;0.475;0.95;1"
+              repeatCount="indefinite"
+              values="0;-16;-59;-59"
+            ></animate>
+          </circle>
+          <animateTransform
+            attributeName="transform"
+            dur="2.5s"
+            repeatCount="indefinite"
+            type="rotate"
+            values="0 12 12;360 12 12"
+          ></animateTransform>
+        </g>
+      </svg>
     </div>
   );
 };
