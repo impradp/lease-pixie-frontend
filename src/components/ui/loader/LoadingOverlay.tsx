514acfcb3b7617091aefb134a6a41df9cab42036
Pradip Puri <impradp@gmail.com>
2025-05-19 14:56:19 +0545

    fix-loader/dots-scale: Simulating dots-scale as loader.

--- src/components/ui/loader/LoadingOverlay.tsx
+++ src/components/ui/loader/LoadingOverlay.tsx
@@ -1,12 +1,43 @@
 "use client";
 
 import React from "react";
-import LoadingSpinner from "@/components/ui/loader/LoadingSpinner";
 
-const LoadingOverlay = ({ size = 50 }: { size?: number }) => {
+const LoadingOverlay = () => {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center">
-      <LoadingSpinner size={size} />
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={48}
+        height={48}
+        viewBox="0 0 24 24"
+      >
+        <circle cx={4} cy={12} r={3} fill="#aa5a36">
+          <animate
+            id="svgSpinners3DotsScale0"
+            attributeName="r"
+            begin="0;svgSpinners3DotsScale1.end-0.313s"
+            dur="0.938s"
+            values="3;.2;3"
+          ></animate>
+        </circle>
+        <circle cx={12} cy={12} r={3} fill="#aa5a36">
+          <animate
+            attributeName="r"
+            begin="svgSpinners3DotsScale0.end-0.75s"
+            dur="0.938s"
+            values="3;.2;3"
+          ></animate>
+        </circle>
+        <circle cx={20} cy={12} r={3} fill="#aa5a36">
+          <animate
+            id="svgSpinners3DotsScale1"
+            attributeName="r"
+            begin="svgSpinners3DotsScale0.end-0.563s"
+            dur="0.938s"
+            values="3;.2;3"
+          ></animate>
+        </circle>
+      </svg>
     </div>
   );
 };
